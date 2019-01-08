c57a4d9f7b1048c2b59f8bb5c7f82067ca9f0145
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-08 15:30:24 -0500

    Chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    var arrTotal = []
+    var index = 0
+
+    while(index < array.length) {
+        arrTotal.push(array.slice(index, index + size))
+        index += size
+    }
+    return arrTotal
+
+    // var arrNew = []
+    // var arrTotal = []
+    // var count = 1
+
+    // for(var i = 0 ; i<array.length ; i++) {
+    //     if(count < size) {
+    //         arrNew.push(array[i])
+    //         if(i == array.length -1) {
+    //             arrTotal.push(arrNew)
+    //         }
+    //         count ++
+    //     } else {
+    //         arrNew.push(array[i])
+    //         arrTotal.push(arrNew)
+    //         arrNew = []
+    //         count = 1
+    //     } 
+    // }
+    // return arrTotal
+
+}
+
+// console.log(chunk([1,2,3], 2))
 
 module.exports = chunk;
