3b8a9a040db83f1d5a897b89963f8d047f65fbff
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-09 14:58:07 -0500

    printing steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+  for (let i = 0; i < n; i++) {
+    let stair = '';
+
+    for (let j = 0; j < n; j++) {
+      if (j <= i) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+
+    console.log(stair);
+  }
+
+    // for(var i = 1 ; i<=n ; i++) {
+    //     var space = ' '
+    //     if(i < n) {
+    //         for(var j = n ; j>i+1 ; j--) {
+    //             var space = space + ' '
+    //         }
+    //     } else {
+    //         space = ''
+    //     }
+        
+    //     var step = ''
+    //     for(var j = 1 ; j<=i; j++) {
+    //         var step = step + '#'
+    //     }
+    //     console.log(step + space)
+    // }
+}
+
+// steps(4)
 
 module.exports = steps;
